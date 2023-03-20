713e59d2da7672aa99a2598a811c973b5bda4e54
hol3ng <savinyhal@gmail.com>
2023-03-20 10:14:30 +0300

    init

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link application.css
+//= link application.js
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
---
+++ db/migrate/20230125073955_add_users_status.rb
@@ -0,0 +1,5 @@
+class AddUsersStatus < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :status, :boolean, default: false
+  end
+end
